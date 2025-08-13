84ff27934e1f2b6e9f0620e347abad9de12be898
Thamire334 <paula.gibrim@ufv.br>
2025-08-13 16:33:13 -0300

    feat: update home

---
+++ README.md
@@ -0,0 +1,9 @@
+## Ambiente de desenvolvimento
+
+> https://curso.dev/web/ambiente-de-desenvolvimento-windows
+
+Depois, com o node instalado:
+
+> nvm install
+
+> npm run dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
-    return <h1>Fiz</h1>
+    return <h1>Eduardinho te amo bonitinho</h1>
 }
+
 
 // Especifica o default padrão -> A partir dele, um componente vai chamar os outros
 export default Home; 
