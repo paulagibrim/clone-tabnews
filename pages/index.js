154a330b9516c8c08fdd96f09e6dfe891c8c8549
Gibrim <paula.gibrim@ufv.br>
2025-08-14 12:01:22 -0300

    feat: add  scripts  and

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 function Home() {
-    return <h1>Oi jessica, vc chegou atrasada hoje ein safadinha</h1>
+  return <h1>Oi jessica, vc chegou atrasada hoje ein safadinha</h1>;
 }
 
-
 // Especifica o default padrão -> A partir dele, um componente vai chamar os outros
-export default Home; 
+export default Home;
